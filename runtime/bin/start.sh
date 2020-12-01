4e7f6d49abed0694c2ba075a9dd249f1c97b55d9
huangxin <572780350@qq.com>
2020-12-01 10:35:30 +0800

    修改jvm参数

--- runtime/bin/start.sh
+++ runtime/bin/start.sh
@@ -39,7 +39,7 @@ fi
 JAVA_MEM_OPTS=""
 BITS=`java -version 2>&1 | grep -i 64-bit`
 if [ -n "$BITS" ]; then
-    JAVA_MEM_OPTS=" -server -Xmx400m -Xms400m -Xmn400m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
+    JAVA_MEM_OPTS=" -server -Xmx512m -Xms512m -Xmn256m -Xss512k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
 else
     JAVA_MEM_OPTS=" -server -Xms400m -Xmx400m -XX:PermSize=256m -XX:MaxPermSize=512M -XX:SurvivorRatio=2 -XX:+UseParallelGC "
 fi
